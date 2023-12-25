f871ecc09a193f8485021a9add606edf91381c6e
pilla gowtham venkatesh <sepl.gowthamvenkatesh@gmail.com>
2023-12-25 17:25:20 +0530

    new hhh

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ const cors = require("cors")
 const TaskRouter  = require("./routes/taskroutes")
 
 const PORT = process.env.PORT || 5000
-app.use(cors({
-    origin: ["*"],
-    optionsSuccessStatus: 200,
-}));
+app.use(cors());
 
 app.use(express.json())
 app.use("/api/task",TaskRouter)
